8ad3d8bba85dd73c0e059f4599b730dbf039a42c
LarsonLaugh <41006386+LarsonLaugh@users.noreply.github.com>
2019-03-18 15:33:55 +0100

    Add data_import and data_process

---
+++ data_import.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import numpy as np
+# path = input()
+path = '//system01\QT\Lixian\data\QC 0492 0h\QC 0492 0H-difTemp\gatesweep\B-3T-Extn1mV_I_38_Rxx_BE_Rxy_BC_Gate4tom6V (02)'
+os.chdir(path)
+
+fname = sorted(glob.glob('TempControl*.dat')) # sort the filenames by their value
+print('dictionary keys:')
+Dic_data = {}
+
+class sData:  # define a class type as a container
+            def __init__(self, Name, Gatevoltage, Temp, Rxx,Rxy):
+                self.name = Name
+                self.gVolt = Gatevoltage
+                self.tmp = Temp
+                self.rxx = Rxx
+                self.rxy = Rxy
+
+for f in fname:
+            read_data = np.loadtxt(f,skiprows=32,usecols=(0,3,7,-2))
+            read_data = np.array(read_data)
+            Dic_data[f] = sData(f,read_data[:,0],read_data[:,1],read_data[:,2],read_data[:,3])
+            print(f)
+
---
+++ data_process.py
@@ -0,0 +1,68 @@
+# data import
+import data_import
+import numpy as np
+import matplotlib.pyplot as plt
+from data_import import fname
+from data_import import Dic_data
+
+e = 1.6021766208E-19
+h = 6.62607015E-34
+# data process
+
+# choose dataset for Rxx - temp dependence plot
+Voltpick = [0,1.2]
+
+# plot
+newf = plt.figure(figsize=(15,18)) # Create a new figure
+ax_rxx = newf.add_subplot(221)
+ax_rxy = newf.add_subplot(222)
+ax_temp = newf.add_subplot(223)
+ax_tlog = newf.add_subplot(224)
+
+# plot Rxx-temp dependence
+for volt in Voltpick:
+       index = 0
+       Temp = [None]*len(fname)
+       R = [None]*len(fname)
+       for f in fname:
+                index_pick = min(range(len(Dic_data[f].gVolt)), key=lambda k: abs(Dic_data[f].gVolt[k]-volt))
+                #print('len(Dic_data[f].gVolt)={}'.format(len(Dic_data[f].gVolt)))
+                #print('index_pick={}'.format(index_pick))
+                Temp[index] = np.mean(Dic_data[f].tmp)
+                R[index] = Dic_data[f].rxx[index_pick]
+                index+=1
+       # plot every single curve in outer loop
+       ax_temp.scatter(Temp,R,label='Vg = %02.2fV'% volt)
+       # plot in in Temp**power semilog
+       ax_tlog.scatter([x**-0.5 for x in Temp],R,label='Vg = %02.2fV'% volt)
+       ax_tlog.set_yscale('log')
+
+ax_temp.set_xlabel('$Temp (K)$')
+ax_temp.set_ylabel('Rxx ($\Omega$)')
+ax_temp.legend()
+ax_tlog.set_xlabel('$Temp^{-1/2} (K)$')
+ax_tlog.set_ylabel('Rxx ($\Omega$)')
+ax_tlog.legend()
+
+# plot Rxx
+fname = sorted(fname,key = lambda x: np.mean(Dic_data[x].tmp)) # sort fname by its temp value
+for f in fname:
+       ax_rxx.plot(Dic_data[f].gVolt,Dic_data[f].rxx,label='Temp = %02.1fK'% np.mean(Dic_data[f].tmp))
+ax_rxx.set_xlabel('Gate Voltage (V)')
+ax_rxx.set_ylabel('Rxx ($\Omega$)')
+ax_rxx.legend()
+for volt in Voltpick:
+        ax_rxx.axvline(x=volt, linestyle=':', color='c')
+
+# plot Rxy
+for f in fname:
+       ax_rxy.plot(Dic_data[f].gVolt,Dic_data[f].rxy,label='Temp = %02.1fK'% np.mean(Dic_data[f].tmp))
+ax_rxy.set_xlabel('Gate Voltage (V)')
+ax_rxy.set_ylabel('Rxy ($\Omega$)')
+ax_rxy.legend()
+for volt in Voltpick:
+        ax_rxy.axvline(x = volt, linestyle = ':', color = 'c')
+for i in range(2,5):
+        ax_rxy.axhline(y = h/i/e**2,linestyle = ':',color = 'm')
+plt.show()
+newf.savefig('plot_insulatorlike.pdf')
