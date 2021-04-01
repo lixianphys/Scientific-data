6b9313de059a3d1d0a66c308bfc7d985685c849e
Lixian Wang <lixian.wang@physik.uni-wuerzburg.de>
2021-04-01 09:54:09 +0200

    m

--- script.ps1
+++ script.ps1
@@ -4,5 +4,14 @@
 # python toybands/run.py -denplot -fnm den_12 --enrange -0.2 0.1 100 --bfrange 1 7 120 -nmax 10
 # python toybands/run.py -enplot -fnm en_8 --enrange -0.2 0.1 100 --bfrange 1 7 120 -nmax 10
 
+python toybands/delband.py -i all
+python toybands/addband.py 1e15 -is_dirac -is_cond -gfactor 10 -dp 0 403365
+python toybands/addband.py 1e15 -is_dirac -is_cond -gfactor 10 -dp 0 403365
+python toybands/addband.py 1e15 -gfactor 6 -cp 0.2 1
+python toybands/run.py -simu -loadden output/density.csv -fnm simu_g10 --enrange -0.05 0.1 50 --bfrange 1 7 120  -nmax 10
+python toybands/delband.py -i all
+python toybands/addband.py 1e15 -is_dirac -is_cond -gfactor 28 -dp 0 403365
+python toybands/addband.py 1e15 -is_dirac -is_cond -gfactor 28 -dp 0 403365
+python toybands/addband.py 1e15 -gfactor 6 -cp 0.2 1
+python toybands/run.py -simu -loadden output/density.csv -fnm simu_g28 --enrange -0.05 0.1 50 --bfrange 1 7 120  -nmax 10
 
-python toybands/run.py -simu -loadden output/density.csv -fnm simu_g1p8 --enrange -0.05 0.1 50 --bfrange 1 7 120  -nmax 10
--- toybands/delband.py
+++ toybands/delband.py
@@ -6,27 +6,57 @@ import pandas as pd
 from toybands.functions import pretty_print
 
 
-if __name__ == "__main__":
-    if os.path.isfile("system.json"):
-        df = pd.read_json("system.json")
-        if not df.empty:
-            print("Your current system:")
-            pretty_print(df)
-            index = input("which band to delete? input the index number: Or press 'e' to exit, 'all' to clear up\n")
-            if index == 'e':
-                sys.stderr.write('Exit...\n')
-                exit()
-            elif index == 'all':
-                df.drop(df.index).to_json("system.json")
-                print('No bands in your system, please add more using addband.py')
-                exit()
+def get_args():
+    my_parser = argparse.ArgumentParser(
+        prog="delband", description="Delete a band or all bands"
+    )
+    my_parser.add_argument(
+    "-i",
+    action="store",
+    type=str,
+    help="index number of band to delete"
+    )
+    args = my_parser.parse_args()
+    return args
+
+def no_ui(df,index):
+    if index == 'e':
+        sys.stderr.write('Exit...\n')
+    elif index == 'all':
+        df.drop(df.index).to_json("system.json")
+        print('No bands in your system, please add more using addband.py')
+    else:
+        df.drop([int(index)]).to_json("system.json")
+
+def with_ui(df):
+    if not df.empty:
+        print("Your current system:")
+        pretty_print(df)
+        index = input("which band to delete? input the index number: Or press 'e' to exit, 'all' to clear up\n")
+        if index == 'e':
+            sys.stderr.write('Exit...\n')
+        elif index == 'all':
+            df.drop(df.index).to_json("system.json")
+            print('No bands in your system, please add more using addband.py')
+        else:
             print("Deleted No.", index, "band")
             df.drop([int(index)]).to_json("system.json")
             if not df.drop([int(index)]).empty:
                 print("Now, your system:")
                 pretty_print(df.drop([int(index)]))
+            else:
+                print('No bands in your system, please add more using addband.py\n')
+    else:
+        print('No bands in your system, please add more using addband.py\n')
+
+if __name__ == "__main__":
+    if os.path.isfile("system.json"):
+        df = pd.read_json("system.json")
+        args = get_args()
+        if args.i is not None:
+            no_ui(df,args.i)
         else:
-            print('No bands in your system, please add more using addband.py\n')
+            with_ui(df)
     else:
         sys.stderr.write("No system exist\n")
         exit()
